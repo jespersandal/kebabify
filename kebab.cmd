5e26391438a7da35e41e9c75e99b4c62882dec42
Jesper S. Sandal <jespersandal@outlook.com>
2019-04-17 17:19:02 +0200

    Upload of first working version
    It accepts only one file as input. Basic substitution of spaces and Danish characters. Mixes standard Windows CMD batch with Powershell, although everything could be done with just CMD.

---
+++ kebab.cmd
@@ -0,0 +1,21 @@
+@echo off
+chcp 65001
+setlocal
+
+set ofile=%~nxp1
+set oext=%~x1
+set fstring=%~n1
+set dkfstring=%fstring:ø=oe%
+set dkfstring=%dkfstring:Æ=Ae%
+set dkfstring=%dkfstring:Ø=Oe%
+set dkfstring=%dkfstring:Å=Aa%
+
+Powershell -NoProfile -Command ^
+    $env:dkfstring = $env:dkfstring.replace(' ', '-'); ^
+    $env:dkfstring = $env:dkfstring.replace('æ', 'ae'); ^
+    $env:dkfstring = $env:dkfstring.replace('å', 'aa'); ^
+    $env:dkfstring = $env:dkfstring + $env:oext; ^
+    Rename-Item -Path $env:ofile -NewName $env:dkfstring; ^
+
+pause
+endlocal
